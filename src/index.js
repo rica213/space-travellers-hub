00662fca8783ba73def4863d5f4856bc5d2d957c
jonas-45 <jonaswullah@gmail.com>
2023-03-13 14:56:53 +0000

    Import react bootstrap into index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './app/store';
 import App from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/react-bootstrap/dist/react-bootstrap';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
