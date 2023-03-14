db5ad9a995b89652bae49b56436dc1263a7f4a58
jonas-45 <jonaswullah@gmail.com>
2023-03-14 13:43:38 +0000

    Use useselector to get rocket list from state

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const rockets = () => (
-  <div>rockets</div>
-);
+const rockets = () => {
+  const { rockets } = useSelector((state) => state.rocket);
+  return (
+    <div>rockets</div>
+  )
+}
 
 export default rockets;
