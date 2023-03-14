fa73181679e3a4c5b0e7336cabbb801e634e579d
jonas-45 <jonaswullah@gmail.com>
2023-03-14 11:32:27 +0000

    Fetch data from spacex api

--- src/redux/rockets/rocketSlice.js
+++ src/redux/rockets/rocketSlice.js
@@ -1,6 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { async } from "q";
 
+const url = 'https://api.spacexdata.com/v3/rockets';
 
+const getRockets = createAsyncThunk('rockets/data', async () => {
+  const rocketsData = await fetch(url).then((resp) => resp.json()).catch(() => false);
+  rocketsData.forEach(rocketObj => {
+    return {
+      id: rocketObj.rocket_id,
+      name: rocketObj.rocket_name,
+      type: rocketObj.rocket_type,
+      describe: rocketObj.description,
+      flickr_images: rocketObj.flickr_images,
+      reserved: false
+    }
+  });
+
+});
 
 const initialState = {
   rockets: [
