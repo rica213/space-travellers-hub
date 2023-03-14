d722eb19d18918fd4ec4497da3917e0db14ed8c8
jonas-45 <jonaswullah@gmail.com>
2023-03-14 06:20:21 +0000

    Import store into index.js file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import store from './app/store';
+import store from './redux/store';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/react-bootstrap/dist/react-bootstrap';
