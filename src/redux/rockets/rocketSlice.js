66fcda580be5d0ccd701ee534252137fce85bd78
jonas-45 <jonaswullah@gmail.com>
2023-03-14 13:01:23 +0000

    Returned needed data from fetched rockets array

--- src/redux/rockets/rocketSlice.js
+++ src/redux/rockets/rocketSlice.js
@@ -1,39 +1,36 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { async } from "q";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const url = 'https://api.spacexdata.com/v3/rockets';
 
-const getRockets = createAsyncThunk('rockets/data', async () => {
-  const response = await fetch(url).then((resp) => resp.json()).catch(() => false);
-  const rocketsData = response.forEach(rocketObj => {
-    return {
-      id: rocketObj.rocket_id,
-      name: rocketObj.rocket_name,
-      type: rocketObj.rocket_type,
-      describe: rocketObj.description,
-      flickr_images: rocketObj.flickr_images,
-      reserved: false
-    }
-  });
-  return rocketsData
+export const getRockets = createAsyncThunk('rockets/data', async () => {
+  console.log('Starting fetching');
+  const response = await fetch(url).then((resp) => resp.json()).catch((error) => console.log(error));
+  const rocketsData = await response.map((rocketObj) => ({
+    id: rocketObj.rocket_id,
+    name: rocketObj.rocket_name,
+    type: rocketObj.rocket_type,
+    describe: rocketObj.description,
+    flickr_images: rocketObj.flickr_images[0],
+    reserved: false,
+  }));
+  
+  return rocketsData;
 });
 
 const initialState = {
-  rockets: []
-}
+  rockets: [],
+};
 
 const rocketSlice = createSlice({
   name: 'rockets',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getRockets.fulfilled, (state, action) => {
-      return {
-        ...state,
-        rockets: action.payload
-      }
-    });
-  }
-})
+    builder.addCase(getRockets.fulfilled, (state, action) => ({
+      ...state,
+      rockets: action.payload,
+    }));
+  },
+});
 
 export default rocketSlice.reducer;
