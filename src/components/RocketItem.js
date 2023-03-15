f36725b5f9c078a5528b9c71c47d56c9d0016a8c
jonas-45 <jonaswullah@gmail.com>
2023-03-15 07:55:02 +0000

    Add click listener to reservation button

--- src/components/RocketItem.js
+++ src/components/RocketItem.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { reserveRocket } from '../redux/rockets/rocketSlice';
 
 const RocketItem = ({ rocket }) => {
+  const dispatch = useDispatch();
   const {
-    name, description, flickrImage, reserved,
+    id, name, description, flickrImage, reserved,
   } = rocket;
+
+  const handleRocketReservation = (id) => {
+    dispatch(reserveRocket(id));
+  };
 
   return (
     <div className="d-flex flex-row gap-3 px-5 mb-4">
       <img className="rocket-img" src={flickrImage} alt={name} />
       <div>
         <h3>{name}</h3>
-        <p>
+        <p className="description">
           {(reserved && (<span className="reserved">Reserved</span>))}
           {' '}
           {description}
         </p>
         <div>
-          {(reserved && (<button type="button" className="reserved-btn">Cancel Reservation</button>))}
-          {(!reserved && (<button type="button" className="btn btn-primary">Reserve Rocket</button>))}
+          {(reserved && (<button type="button" className="btn btn-secondary" style={{ backgroundColor: '#fff', color: '#70777F' }}>Cancel Reservation</button>))}
+          {(!reserved && (<button type="button" className="btn btn-primary" onClick={() => handleRocketReservation(id)}>Reserve Rocket</button>))}
         </div>
       </div>
     </div>
@@ -27,6 +34,7 @@ const RocketItem = ({ rocket }) => {
 
 RocketItem.propTypes = {
   rocket: PropTypes.shape({
+    id: PropTypes.string,
     name: PropTypes.string,
     description: PropTypes.string,
     flickrImage: PropTypes.string,
