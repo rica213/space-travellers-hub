5c4296a4a17de1bdc72af3444cabc5170e6a7686
jonas-45 <jonaswullah@gmail.com>
2023-03-15 18:11:21 +0000

    Refactor Rockets component to display loading spinner whilst loading api data

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -4,11 +4,22 @@ import RocketItem from '../components/RocketItem';
 
 const Rockets = () => {
   const { rockets } = useSelector((state) => state.rocket);
+  const isLoading = useSelector((state) => state.rocket.isLoading);
   return (
     <div>
-      {rockets.map((rocketObj) => (
-        <RocketItem rocket={rocketObj} key={rocketObj.id} />
+      {(isLoading && (
+      <div style={{
+        display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh', paddingBottom: '20%',
+      }}
+      >
+        <div className="spinner-border text-warning" role="status">
+          <span className="sr-only" />
+        </div>
+      </div>
       ))}
+      {!isLoading && (rockets.map((rocketObj) => (
+        <RocketItem rocket={rocketObj} key={rocketObj.id} />
+      )))}
     </div>
   );
 };
