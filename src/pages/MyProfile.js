f921c045590163d87e5d3c3705c247075256828f
jonas-45 <jonaswullah@gmail.com>
2023-03-15 11:07:15 +0000

    Filter rocket list to get only reserved rockets

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <div>MyProfile</div>
-);
+const MyProfile = () => {
+  const { rockets } = useSelector((state) => state.rocket);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved === true);
+  return (
+    <div>
+      <div className="reserved-rockets">
+        <h2>My Rockets</h2>
+        
+        
+      </div>
+  </div>
+  )
+};
 
 export default MyProfile;
