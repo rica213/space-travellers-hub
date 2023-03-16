074cfb29b031877578e497f968e1af39d15ac236
rica213 <25f5k252l@mozmail.com>
2023-03-16 16:34:04 +0300

    Fix eslint errors

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,40 +1,40 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Header from "../components/Header";
-import { BrowserRouter as Router } from "react-router-dom";
-import "@testing-library/jest-dom/extend-expect";
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from '../components/Header';
+import '@testing-library/jest-dom/extend-expect';
 
-describe("Header component", () => {
-  it("should contain the logo", () => {
+describe('Header component', () => {
+  it('should contain the logo', () => {
     const { getByAltText } = render(
       <Router>
         <Header />
-      </Router>
+      </Router>,
     );
-    const logo = getByAltText("space traveller hub logo");
+    const logo = getByAltText('space traveller hub logo');
     expect(logo).toBeInTheDocument();
   });
 
-  it("should contain the app name", () => {
+  it('should contain the app name', () => {
     const { getByText } = render(
       <Router>
         <Header />
-      </Router>
+      </Router>,
     );
     const appName = getByText("Space Travelers' Hub");
     expect(appName).toBeInTheDocument();
   });
 
-  it("should render NavbarItem component", () => {
+  it('should render NavbarItem component', () => {
     const { getByRole, getByTestId } = render(
       <Router>
         <Header />
-      </Router>
+      </Router>,
     );
-    const navbarItem = getByTestId("navbar-item");
-    const rockets = getByRole("link", { name: "Rockets" });
-    const missions = getByRole("link", { name: "Missions" });
-    const myProfile = getByRole("link", { name: "My Profile" });
+    const navbarItem = getByTestId('navbar-item');
+    const rockets = getByRole('link', { name: 'Rockets' });
+    const missions = getByRole('link', { name: 'Missions' });
+    const myProfile = getByRole('link', { name: 'My Profile' });
 
     expect(navbarItem).toBeInTheDocument();
     expect(rockets).toBeInTheDocument();
